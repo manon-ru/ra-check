61a42e530f1491011a0e4fa54748b9af56477862
Manon Leijser <manon.leijser@ru.nl>
2026-02-27 15:51:00 +0100

    Add files via upload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "405964970" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3324795823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
